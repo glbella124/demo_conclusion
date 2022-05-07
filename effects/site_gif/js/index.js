47c32074dcec94f6319544db89ca7e68541f1567
Bella <896848118@qq.com>
2022-05-07 23:29:29 +0800

    commit effects

---
+++ effects/site_gif/js/index.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    // TweenMax.to("#rect", 3, { left: 500 });
+    let video = document.querySelector("#site-video")
+    if (video) {
+        video.play()
+            // console.log(video)
+    }
+
+}
